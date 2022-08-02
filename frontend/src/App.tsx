aa2c4afbc642457cf7bc8d5d7552a5ebca4173de
jin <jingyukang91@gmail.com>
2022-08-02 16:15:32 +1200

    Frontend - step 1&2 Start coding & Building to basic UI to take user input

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("");
+
+  const search = (): void => {
+    alert("Search button has been clicked!");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon Search</h1>
+
+      <div>
+        <label>Pokemon Name</label> <br />
+        <input
+          type="text"
+          id="pokemon-name"
+          name="pokemon-name"
+          onChange={(e) => setPokemonName(e.target.value)}
+        />
+        <br />
+        <button onClick={search}>Search</button>
+      </div>
+
+      <p>You have entered {pokemonName}</p>
     </div>
   );
 }
